198eaa10a7189dbb387272661091d3bd9e354c98
0x5a4 <1444@wienstroer.net>
2025-11-16 20:05:39 +0100

    update flake.lock

--- mod/home-manager/git.nix
+++ mod/home-manager/git.nix
@@ -13,6 +13,19 @@
       gh
     ];
 
+    programs.delta = {
+      enable = true;
+      enableGitIntegration = true;
+      
+      options = {
+        navigate = true;
+        light = false;
+        line-numbers = true;
+        line-numbers-left-format = "{nm:>4} │";
+        colorMoved = "default";
+      };
+    };
+    
     programs.git =
       let
         keySelector = pkgs.writers.writeBashBin "select-ssh-sign-key" ''
@@ -20,23 +33,10 @@
         '';
       in
       {
-        userName = "0x5a4";
-        userEmail = "1444@wienstroer.net";
-
         enable = true;
 
         lfs.enable = true;
 
-        delta = {
-          enable = true;
-          options = {
-            navigate = true;
-            light = false;
-            line-numbers = true;
-            line-numbers-left-format = "{nm:>4} │";
-            colorMoved = "default";
-          };
-        };
 
         attributes = [
           "* text=auto"
@@ -47,15 +47,40 @@
           signByDefault = true;
         };
 
-        extraConfig = {
-          gpg.ssh.defaultKeyCommand = lib.getExe keySelector;
+        settings = {
+          user = {
+            name = "0x5a4";
+            email = "1444@wienstroer.net";
+          };
+          
           core = {
             whitespace = "trailing-space,space-before-tab";
             eol = "lf";
           };
+
+          push = {
+            followTags = true;
+            autoSetupRemote = true;
+          };
+
+          rebase = {
+            autoStash = true;
+            abbreviateCommands = true;
+          };
+
+          alias = {
+            exec = "!exec ";
+            make = "!exec make ";
+            fuckup = "reset --soft HEAD~1";
+            root = "rev-parse --show-toplevel";
+            lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+            fpush = "push --force-with-lease";
+            smartblame = "blame -w -CCC";
+          };
+
+          
           pull.rebase = true;
-          push.followTags = true;
-          push.autoSetupRemote = true;
+          
           feature.manyFiles = true;
           init.defaultBranch = "main";
           column.ui = "auto";
@@ -63,20 +88,9 @@
           diff.colorMoved = "default";
           advice.addEmptyPathspec = false;
           commit.gpgSign = true;
-          rebase.autoStash = true;
-          rebase.abbreviateCommands = true;
           rerere.enabled = true;
           branch.sort = "-committerdate";
-        };
-
-        aliases = {
-          exec = "!exec ";
-          make = "!exec make ";
-          fuckup = "reset --soft HEAD~1";
-          root = "rev-parse --show-toplevel";
-          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-          fpush = "push --force-with-lease";
-          smartblame = "blame -w -CCC";
+          gpg.ssh.defaultKeyCommand = lib.getExe keySelector;
         };
       };
   };
